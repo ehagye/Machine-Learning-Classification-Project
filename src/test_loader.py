78b41410c51421ac1e9fe997f851479a5e6f6a71
Emery Hagye <130592456+ehagye@users.noreply.github.com>
2025-12-05 15:28:08 -0500

    Delete src/test_loader.py

--- src/test_loader.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-from src.data_loader import load_dataset
-
-data = load_dataset("data/TrainData1.txt", "data/TrainLabel1.txt", "data/TestData1.txt")
-
-print(data["X_train"].shape)
-print(data["y_train"][:5])
-print(np.isnan(data["X_train"]).sum())
